1bf9267734200d9832d87d934f1e6b0bd4d8a072
xiongjinchao <xiongjinchao@baicheng.com>
2018-04-10 16:54:25 +0800

    header

--- resources/views/layouts/admin-lte/header.blade.php
+++ resources/views/layouts/admin-lte/header.blade.php
@@ -33,7 +33,7 @@
                                     <a href="#">
                                         <div class="pull-left">
                                             <!-- User Image -->
-                                            <img src="{{asset("images/logo.jpg")}}" class="img-circle" alt="User Image">
+                                            <img src="{{asset("images/logo-white.jpg")}}" class="img-circle" alt="User Image">
                                         </div>
                                         <!-- Message title and timestamp -->
                                         <h4>
